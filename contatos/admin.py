27be74e85a13cdb8d3fccb913953b7c3377a9dc8
levi-sr93 <levi-sr@outlook.com>
2021-02-15 18:17:23 -0300

    Usando condicionais| exibindo usuarios baseado na condição no template

--- contatos/admin.py
+++ contatos/admin.py
@@ -6,11 +6,12 @@ from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome',
-                    'telefone', 'email', 'data_criacao', 'categoria')
+                    'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     # list_filter = ('nome', 'sobrenome')
     list_per_page = 5
     search_fields = ('nome', 'sobrenome', 'telefone')
+    list_editable = ('telefone', 'mostrar')
 
 
 admin.site.register(Categoria)
